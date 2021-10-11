0692f8c93e80e513521c7dae4ce4e4f24ef31eb7
Rafael Takei <rafael@suthub.com>
2021-10-11 00:00:57 -0300

    Initial Setup

---
+++ components/Tutorial.vue
@@ -0,0 +1,21 @@
+<!-- Please remove this file from your project -->
+<template>
+  <div class="text-4xl">
+    Hello, {{ authStore.name }}! {{ test }} {{ number }}
+  </div>
+</template>
+
+<script setup lang="ts">
+import { useAuthStore } from '~/store/auth'
+
+const authStore = useAuthStore()
+const test = ref('5')
+defineProps({
+  number: {
+    type: Number,
+    default: 0,
+  },
+})
+</script>
+
+<style scoped></style>
---
+++ pages/index.vue
@@ -0,0 +1,9 @@
+<template>
+  <Tutorial :number="10" />
+</template>
+
+<script setup lang="ts">
+onMounted(() => {
+  console.log('test')
+})
+</script>
